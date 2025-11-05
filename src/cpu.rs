9586aeb365cbba50b391677ad9712a62dc53a8dd
joaoxyz <jpreis808@gmail.com>
2025-11-05 11:24:36 -0300

    Set flags when executing instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -36,8 +36,8 @@ impl Default for CPU {
 }
 
 impl CPU {
-    pub fn load_rom(&mut self, rom: &[u8]) {
-        self.ram[..rom.len()].copy_from_slice(rom);
+    pub fn load_rom(&mut self, rom: &[u8], pos: usize) {
+        self.ram[pos..rom.len()].copy_from_slice(rom);
     }
 
     fn get_low(reg: u16) -> u8 {
@@ -60,61 +60,74 @@ impl CPU {
 
     pub fn decode(&mut self, opcode: u8) {
         match opcode {
-            0x00 => (), // nop
-            0x76 => (), // halt
+            0x00 => todo!(), // nop
+            0x76 => todo!(), // halt
             0xCB => {
                 // CB prefixed instuctions
                 let new_opcode = self.fetch();
                 self.decode_prefixed(new_opcode);
             },
-            0x40..=0x7F => (), // load
+            0x40..=0x7F => todo!(), // load
             0x80..=0x87 => {
                 // add
                 let register = opcode & 0b00000111;
-                let rhs_value = self.decode_alu(register);
+                let rhs_value = self.decode_alu_operand(register);
                 self.inst_add(rhs_value);
             },
-            0x88..=0x8F => (), // add with carry
+            0x88..=0x8F => {
+                // add with carry
+                let register = opcode & 0b00000111;
+                let rhs_value = self.decode_alu_operand(register);
+                self.inst_add_carry(rhs_value);
+            }, 
             0x90..=0x97 => {
                 // sub
                 let register = opcode & 0b00000111;
-                let rhs_value = self.decode_alu(register);
+                let rhs_value = self.decode_alu_operand(register);
                 self.inst_sub(rhs_value);
             },
-            0x98..=0x9F => (), // sub with carry
+            0x98..=0x9F => {
+                // sub with carry
+                let register = opcode & 0b00000111;
+                let rhs_value = self.decode_alu_operand(register);
+                self.inst_sub_carry(rhs_value);
+            }, 
             0xA0..=0xA7 => {
                 // and
                 let register = opcode & 0b00000111;
-                let rhs_value = self.decode_alu(register);
+                let rhs_value = self.decode_alu_operand(register);
                 self.inst_and(rhs_value);
             },
             0xA8..=0xAF => {
                 // xor
                 let register = opcode & 0b00000111;
-                let rhs_value = self.decode_alu(register);
+                let rhs_value = self.decode_alu_operand(register);
                 self.inst_xor(rhs_value);
             },
             0xB0..=0xB7 => {
                 // or
                 let register = opcode & 0b00000111;
-                let rhs_value = self.decode_alu(register);
+                let rhs_value = self.decode_alu_operand(register);
                 self.inst_or(rhs_value);
             },
             0xB8..=0xBF => {
                 // compare
                 let register = opcode & 0b00000111;
-                let rhs_value = self.decode_alu(register);
+                let rhs_value = self.decode_alu_operand(register);
                 self.inst_compare(rhs_value);
             },
             _ => eprintln!("Unknown opcode: {opcode:#04X}"),
         }
     }
 
+    // Decode opcodes prefixed with $CB
     fn decode_prefixed(&mut self, opcode: u8) {
         todo!()
     }
 
-    fn decode_alu(&self, n: u8) -> u8 {
+    fn decode_alu_operand(&self, n: u8) -> u8 {
+        // Disassembly table for 8-bit registers
+        // https://archive.gbdev.io/salvage/decoding_gbz80_opcodes/Decoding%20Gamboy%20Z80%20Opcodes.html
         match n {
             0b000 => self.b,
             0b001 => self.c,
@@ -128,49 +141,174 @@ impl CPU {
         }
     }
 
-    // TODO: Set flags
+    fn decode_load_operands(&self, x: u8, y: u8) -> (&u8, u8) {
+        let src = match x { 
+            _ => todo!()
+        };
+
+        let dst = match y {
+            _ => todo!()
+        };
+
+        (dst, src)
+    }
+
     fn inst_add(&mut self, value: u8) {
-        let mut flags = 0 as u8;
-        flags |= Flags::Zero as u8;
+        let mut flags: u8 = 0;
+        let (result, carry) = self.a.carrying_add(value, false);
 
-        self.a += value;
-        self.set_flags(flags);
-    }
-
-    fn inst_sub(&mut self, value: u8) {
-        self.a -= value;
-    }
-
-    fn inst_and(&mut self, value: u8) {
-        self.a &= value;
-    }
-
-    fn inst_xor(&mut self, value: u8) {
-        self.a ^= value;
-    }
-
-    fn inst_or(&mut self, value: u8) {
-        let result = self.a | value;
-
-        let mut flags = 0 as u8;
         if result == 0 {
-            flags |= Flags::Zero as u8;
+            flags |= Flags::Zero;
+        }
+
+        let half_carry = ((self.a & 0b1111) + (value & 0b1111)) > 0b1111;
+        if half_carry {
+            flags |= Flags::HalfCarry;
+        }
+
+        if carry {
+            flags |= Flags::Carry;
         }
 
         self.a = result;
-        self.set_flags(flags);
+        self.f = flags;
+    }
+
+    fn inst_add_carry(&mut self, value: u8) {
+        let mut flags: u8 = 0;
+        let current_carry_flag = ((self.f & Flags::Carry) > 0) as u8;
+        let (result, carry0) = self.a.carrying_add(value, false);
+        let (result, carry1) = result.carrying_add(current_carry_flag, carry0);
+
+        if result == 0 {
+            flags |= Flags::Zero;
+        }
+
+        let half_carry = ((self.a & 0b1111) + (value & 0b1111) + (current_carry_flag)) > 0b1111;
+        if half_carry {
+            flags |= Flags::HalfCarry;
+        }
+
+        // if any of the two adds overflow we should set carry
+        let carry = carry0 | carry1;
+        if carry {
+            flags |= Flags::Carry;
+        }
+
+        self.a = result;
+        self.f = flags;
+    }
+
+    fn inst_sub(&mut self, value: u8) {
+        let mut flags: u8 = 0;
+        let (result, carry) = self.a.borrowing_sub(value, false);
+
+        if result == 0 {
+            flags |= Flags::Zero;
+        }
+
+        flags |= Flags::Subtraction;
+
+        let half_carry = (self.a & 0b1111) < (value & 0b1111);
+        if half_carry {
+            flags |= Flags::HalfCarry;
+        }
+
+        if carry {
+            flags |= Flags::Carry;
+        }
+
+        self.a = result;
+        self.f = flags;
+    }
+
+    fn inst_sub_carry(&mut self, value: u8) {
+        let mut flags: u8 = 0;
+        let current_carry_flag = ((self.f & Flags::Carry) > 0) as u8;
+        let (result, carry0) = self.a.borrowing_sub(value, false);
+        let (result, carry1) = result.borrowing_sub(current_carry_flag, carry0);
+
+        if result == 0 {
+            flags |= Flags::Zero;
+        }
+
+        let half_carry = (self.a & 0b1111) < ((value & 0b1111) + (current_carry_flag));
+        if half_carry {
+            flags |= Flags::HalfCarry;
+        }
+
+        // if any of the two subs overflow we should set carry
+        // according to opcode table SBC A, A does not affect the carry flag, look this up later
+        let carry = carry0 | carry1;
+        if carry {
+            flags |= Flags::Carry;
+        }
+
+        self.a = result;
+        self.f = flags;
+    }
+
+    fn inst_and(&mut self, value: u8) {
+        let mut flags: u8 = 0;
+        let result = self.a & value;
+
+        if result == 0 {
+            flags |= Flags::Zero;
+        }
+
+        flags |= Flags::HalfCarry;
+
+        self.a = result;
+        self.f = flags;
+    }
+
+    fn inst_xor(&mut self, value: u8) {
+        let mut flags: u8 = 0;
+        let result = self.a ^ value;
+
+        flags |= Flags::Zero;
+
+        self.a = result;
+        self.f = flags;
+    }
+
+    fn inst_or(&mut self, value: u8) {
+        let mut flags: u8 = 0;
+        let result = self.a | value;
+
+        if result == 0 {
+            flags |= Flags::Zero;
+        }
+
+        self.a = result;
+        self.f = flags;
     }
 
     fn inst_compare(&mut self, value: u8) {
-        let result = self.a - value;
+        let mut flags: u8 = 0;
+        let (result, carry) = self.a.borrowing_sub(value, false);
+
+        if result == 0 {
+            flags |= Flags::Zero;
+        }
+
+        flags |= Flags::Subtraction;
+
+        let half_carry = (self.a & 0b1111) < (value & 0b1111);
+        if half_carry {
+            flags |= Flags::HalfCarry;
+        }
+
+        if carry {
+            flags |= Flags::Carry;
+        }
+
+        self.f = flags;
     }
 
     fn inst_ld(dst: &mut u8, src: u8) {
         *dst = src;
-    }
-
-    fn set_flags(&mut self, flags: u8) {
-        self.f |= flags;
+        todo!()
     }
 }
 
@@ -180,4 +318,17 @@ enum Flags {
     Subtraction = 0b01000000,
     HalfCarry = 0b00100000,
     Carry = 0b00010000,
+}
+
+impl std::ops::BitOrAssign<Flags> for u8 {
+    fn bitor_assign(&mut self, rhs: Flags) {
+        *self |= rhs as u8;
+    }
+}
+
+impl std::ops::BitAnd<Flags> for u8 {
+    type Output = Self;
+    fn bitand(self, rhs: Flags) -> Self::Output {
+        self & rhs as u8
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ use std::io;
 fn main() -> io::Result<()> {
     let boot_rom = fs::read("dmg_boot.bin")?;
 
-    let a = 0b1;
-
     let mut cpu: cpu::CPU = Default::default();
-    cpu.load_rom(&boot_rom);
+    cpu.load_rom(&boot_rom, 0);
 
     for i in 0..200 {
         let a = cpu.fetch();

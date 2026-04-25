e846172d8c5e01dc463d3efa85398a5fa5e6cf89
joaoxyz <jpreis808@gmail.com>
2026-04-25 18:11:48 -0300

    Add debug logs compatible with gameboy-doctor

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[repr(u8)]
 #[derive(Debug)]
 enum Flags {
@@ -90,7 +92,6 @@ trait Eval {
     fn eval(&self, condtion: Condition) -> bool;
 }
 
-#[derive(Debug)]
 pub struct Cpu {
     ram: [u8; 0xFFFF],
 
@@ -105,6 +106,20 @@ pub struct Cpu {
     l: u8,
     sp: u16,
     pc: u16,
+}
+
+impl fmt::Debug for Cpu {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "A:{:02X} F:{:02X} B:{:02X} C:{:02X} D:{:02X} E:{:02X} H:{:02X} L:{:02X} SP:{:04X} PC:{:04X} PCMEM:{:02X},{:02X},{:02X},{:02X}",
+            self.a, self.f, self.b, self.c, self.d, self.e, self.h, self.l, self.sp, self.pc,
+            self.ram[self.pc as usize],
+            self.ram[(self.pc+1) as usize],
+            self.ram[(self.pc+2) as usize],
+            self.ram[(self.pc+3) as usize],
+        )
+    }
 }
 
 impl Default for Cpu {
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let boot_rom = fs::read("dmg_boot.bin")?;
+    let boot_rom = fs::read("roms/dmg_boot.bin")?;
 
     let mut cpu: cpu::Cpu = Default::default();
     cpu.load_rom(&boot_rom, 0);
 
     loop {
         let a = cpu.fetch();
-        println!("{a:#X?}");
         cpu.decode(a);
+        println!("{cpu:?}");
     }
 }
